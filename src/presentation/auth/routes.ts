2460a01cb1dab2f6562c71297d778e53aedbd912
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-03-19 10:08:50 -0400

    Add AuthRoutes

---
+++ src/presentation/auth/routes.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+
+
+
+
+export class AuthRoutes {
+
+
+  static get routes(): Router {
+
+    const router = Router();
+    
+    // Definir las rutas
+    router.post('/login', /*TodoRoutes.routes */ );
+    router.post('/register', /*TodoRoutes.routes */ );
+
+    router.get('/validate-email/:token', /*TodoRoutes.routes */ );
+
+    return router;
+  }
+
+
+}
+
