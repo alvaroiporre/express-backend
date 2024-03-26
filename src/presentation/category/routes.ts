e8c3ceaeb9f98749e48ebfe352eb52857ba59803
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-03-25 20:40:04 -0400

    Add category service

--- src/presentation/category/routes.ts
+++ src/presentation/category/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CategoryController } from "./controller";
+import { AuthMIddleware } from "../middlewares/auth.middleware";
 
 export class CategoryRoutes {
   static get routes(): Router {
@@ -7,7 +8,7 @@ export class CategoryRoutes {
     const controller = new CategoryController();
     // Definir las rutas
     router.get("/", controller.getCategories);
-    router.post("/", controller.createCategory);
+    router.post("/", [ AuthMIddleware.validateJWT ], controller.createCategory);
     return router;
   }
 }
